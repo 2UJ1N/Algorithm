6f0443aecf9535d974f9114d113f29baeb1ab8ac
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2024-02-02 01:20:22 +0900

    update : duplication file

--- BOJ/n11718.py
+++ BOJ/n11718.py
@@ -1,10 +1,23 @@
-#
-# 11718
-# 그대로 출력하기
-# https://www.acmicpc.net/problem/11718
+# #
+# # 11718
+# # 그대로 출력하기
+# # https://www.acmicpc.net/problem/11718
 
-while 1:
-    try:
-        print(input())
-    except:
-        exit()
+# while 1:
+#     try:
+#         print(input())
+#     except:
+#         exit()
+
+a = 3
+b = 5
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sumArr = [0]
+
+for i in range(len(arr)):
+	sumArr.append(sumArr[i] + arr[i])
+
+print(sumArr)
+answer = sumArr[b] - sumArr[a - 1]
+print(answer)
