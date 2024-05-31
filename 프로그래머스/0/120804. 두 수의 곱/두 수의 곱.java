d3e1e9ce51f2a908b0e75ee588e9c71e238c17c5
Youjin Lee <83401978+2UJ1N@users.noreply.github.com>
2024-05-31 23:32:59 +0900

    [level 0] Title: 두 수의 곱, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-        if (num1 > 0 && num2 > 0 && num1 <= 100 && num2 <= 100) {
+        if (num1 > 0 && num2 > 0) {
             answer = num1 * num2;
         }
         return answer;
