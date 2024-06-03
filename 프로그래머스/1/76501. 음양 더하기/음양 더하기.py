1126e2b71f1f65b71a8fd8cf69c84fb4e1d83cc9
Youjin Lee <83401978+2UJ1N@users.noreply.github.com>
2024-06-03 19:15:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,2 @@
 def solution(absolutes, signs):
-    answer = 0
-    for i in range(len(absolutes)):
-        if signs[i] == 1:
-            answer += absolutes[i]
-        else:
-            answer += -1 * absolutes[i]
-    return answer
+    return sum([ab if sign else -ab for ab, sign in zip(absolutes, signs)])
