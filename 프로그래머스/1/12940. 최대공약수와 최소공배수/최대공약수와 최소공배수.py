755a123ba5c20d7ed9037abbb4d2ee2fb79f8696
Youjin Lee <83401978+2UJ1N@users.noreply.github.com>
2024-06-03 23:47:43 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,9 @@
+def solution(n, m):
+    answer = [gcd(n,m), lcm(n,m)]
+    return answer
+
+def gcd(a, b):
+    return b if a % b == 0 else gcd(b, a % b)
+
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
