c1343ecf733f704c21e23688205e48020847059b
Youjin Lee <83401978+2UJ1N@users.noreply.github.com>
2024-06-19 19:17:45 +0900

    [Silver III] Title: 모두의 마블, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+card = list(map(int, input().split()))
+card.sort(reverse=True)
+
+gold = 0
+level = 0
+for i in range(len(card) - 1):
+    if card[i] > level: level = card[i]
+    if card[i + 1] <= level: gold += level + card[i + 1]
+
+print(gold)
