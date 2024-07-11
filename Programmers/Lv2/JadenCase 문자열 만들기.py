c892c6eedb0a6a13f87c7c994f19e00c346a865c
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2024-07-11 14:58:48 +0900

    PRO | JadenCase 문자열 만들기 ✅

---
+++ Programmers/Lv2/JadenCase 문자열 만들기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    word = ''
+    answer = ''
+    for chk in s:
+        if chk != ' ':
+            word += chk
+        else:
+            tmp = word.capitalize()
+            answer += tmp
+            word = ''
+            answer += chk
+    tmp = word.capitalize()
+    answer += tmp
+    
+    return answer
