334459dcfeb4e2c94ab4655a69556bbb2f2733e5
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2024-07-13 15:18:11 +0900

    PRO | LV2 괄호 회전하기 - 100

---
+++ Programmers/Lv2/괄호 회전하기.py
@@ -0,0 +1,34 @@
+def solution(s):
+    cnt = 0
+    if len(s) == 1: return 0
+    elif len(s) == 2:
+        if s == '()' or s == '[]' or s == '{}':
+            return 1
+    else:
+        for i in range(len(s)):
+            target = s[i:] + s[:i]
+            stack = []
+            flag = 1
+            for t in target:
+                if t == '(' or t == '{' or t == '[':
+                    stack.append(t)
+                elif t == ')':
+                    if len(stack) != 0 and stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        flag = 0
+                        break
+                elif t == '}':
+                    if len(stack) != 0 and stack[-1] == '{':
+                        stack.pop()
+                    else:
+                        flag = 0
+                        break
+                elif t == ']':
+                    if len(stack) != 0 and stack[-1] == '[':
+                        stack.pop()
+                    else:
+                        flag = 0
+                        break
+            if len(stack) == 0 and flag == 1: cnt += 1
+        return cnt
