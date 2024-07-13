0f22a71ffc5c0ef1d0411b2ea5d542ed842f31d1
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2024-07-13 15:37:02 +0900

    PRO | LV2 멀리 뛰기 ✅ - 시간 초과

--- Programmers/Lv2/멀리 뛰기.py
+++ Programmers/Lv2/멀리 뛰기.py
@@ -3,11 +3,10 @@ from itertools import permutations
 def solution(n):
     case = []
     for i in range(n // 2 + 1):
-        tmp = []
-        for cnt2 in range(i):
-            tmp.append(2)
-        for cnt1 in range(n - i * 2):
-            tmp.append(1)
+        add2 = [2] * i
+        add1 = [1] * (n - i * 2)
+        
+        tmp = add2 + add1
         case.append(tmp)
 
     answer = 0
@@ -19,4 +18,4 @@ def solution(n):
             chk2 = set(list(permutations(c)))
             answer += len(chk2)
 
-    return answer
+    return answer % 1234567
