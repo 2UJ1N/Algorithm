36b9d6124515ddd974259c8fbf89ee11104d5d66
Youjin Lee <83401978+2UJ1N@users.noreply.github.com>
2025-11-01 01:35:00 +0900

    [level 2] Title: 올바른 괄호, Time: 10.32 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,15 @@
 def solution(s):
-    stack = []
-    for i in s:
-        if i == "(":
-            stack.append(i)
-        else:
-            if len(stack) == 0:
-                return False
+    if len(s) == 0: return False
+    
+    if s[0] == ')' or s[-1] == '(': return False
+    else:
+        remain = []
+        for i in s:
+            if i == '(': remain.append(i)
             else:
-                stack.pop()
-    if len(stack) != 0:
-        return False
+                if len(remain) != 0 and remain[-1] == '(': remain.pop()
+                else: return False
+        if len(remain) == 0: return True
+        else: return False
+
     return True
